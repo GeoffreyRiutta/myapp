66c832e2c70c986396979991dc85873c2c18c369
Geoffrey Riutta <geoffrey.riutta@ccisystems.com>
2018-07-18 16:10:09 -0500

    added some comments to controler to explain.

--- app/controllers/geo_results_controler.rb
+++ app/controllers/geo_results_controler.rb
@@ -1,14 +1,15 @@
 class GeoResultsControler < ApplicationController
     def Create
-      #
+      #right now we are not doing just the search term
       geo_result = geo_result.Create!(geo_result_params)
   
     end
     
     private
     def geo_result_params
-        #
-        params.require(:geo_result).permit(:search, :result)
+        #just search, later we may be attaching the result data if fessiable with
+        #activestorage
+        params.require(:geo_result).permit(:search)
     end 
 end
   
--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -7,6 +7,7 @@ require 'stringio'
 class VisitorsController < ApplicationController
     include KML
 
+    #Test junk
     def zero_island()
         #crate kml file, create zero 'island' pin and set it up for download
         kml = KMLFile.new
@@ -36,45 +37,46 @@ class VisitorsController < ApplicationController
         send_data kml.render, :filename => "cci_out.kml"
 
     end
+    #end of test junk
 
     def cci_search()
         #Create kml file with entered location
         kml = KMLFile.new
         folder = KML::Folder.new(:name => "data")
 
-        puts "???"
-        puts params
-        puts "!!!"
-
+        #the form is test_load. Data is the test_load text form
         result = Geocoder.search(params[:test_load][:data])
-        puts result
 
+        #TODO; yell at user for null results
+        
+        #place the results into of the geocoding into the coords, also name it and
+        #possilby put description data
+        #TODO; Remove CCI hard coding
         folder.features << KML::Placemark.new(
             :name => "CCI",
             :geometry => KML::Point.new(:coordinates => {:lat=> result.first.coordinates[0],:lng=> result.first.coordinates[1]})
         )
-
+        #folder is all the placemarks or other data, shove it into the objects 
         kml.objects << folder 
+        #send the user the fresh KML
         send_data kml.render, :filename => "cci_out.kml"
 
         #create a new geo results
-        #encoding because won't accept straight objects or strings
-        #convert = Base64.encode64(kml.render)
+
         result = GeoResult.create(search: params[:test_load][:data])
-
-        fake_file = StringIO.new(kml.render)
-
-        puts "pre silence"
-        puts result.result.attached?
-        puts "lambs"
         
+        #Welcome to the fun that is the attach. Attach wants a physical disk file
+        #gotten through open method but we do not have that. we have a kml object
+        #or the render output which is string. Neither have the read method that is
+        #required for attachment.
+        #so we force it into StringIO which acts like a file in ram and thus has
+        #read
+        fake_file = StringIO.new(kml.render)
+        #TODO; give it better file name
         result.result.attach(io: fake_file, filename: "test.kml")
-
+        #always close your fake files
         fake_file.close
 
-        puts "the lotion on its skin"
-        puts result.result.attached?
-        puts "or else it gets attached again"
         #result.save
     end
 
